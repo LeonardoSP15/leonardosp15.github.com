f95e9958f817ba10b10284ce947746c9a0cb6a15
LeonardoSP15 <leonardosp0715@gmail.com>
2020-12-27 21:38:05 -0500

    Agregamos el proyecto boletines

---
+++ boletines/assets/scripts/main.js
@@ -0,0 +1,28 @@
+console.log("hola desde un archivo externo de javascript");
+
+//DOM
+ let links = document.querySelectorAll(".close");
+
+ links.forEach(function(link) {
+
+
+     link.addEventListener("click", function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+        //quitar las clases de animación que ya tiene
+        content.classList.remove("animate__bounceInDown");
+        content.classList.remove("animate__animated");
+        //agregar las clases para animar su salida fadeOutUp
+        ontent.add.remove("animate__fadeOutUp");
+        content.add.remove("animate__animated");
+
+        setTimeout(function(){
+            location.href = "/";
+        },600)
+
+
+        return false;
+     });
+ });
+
+ 
